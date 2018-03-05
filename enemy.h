f9e8367d1207e1c310975fba233f9b8e1357a8f5
Quarke <tflanagan94@me.com>
2018-03-05 16:56:39 -0500

    minor changes removing, enemy stuff to provide room for the new version

--- enemy.h
+++ enemy.h
@@ -7,22 +7,27 @@ class Enemy{
 private:
   std::string name;
 
-  std::string power;
-  std::string kill_method;
+  int health_CURRENT;
+  int damage_person;
+  int damage_self;
+  int health_MAX;
 
 public:
-  Enemy(const std::string name, std::string p, std::string k);
+  Enemy(const std::string name, int pdamage, int sdamage, int chealth, int mhealth);
 
   std::string getName();
-  std::string getAbility();
-  std::string getKillMethod();
+  int getDamage_Self();
+  int getDamage_Person();
+  int getMaxHealth();
+  int getCurrentHealth();
 
   void setName(const std::string name);
-  void setAbility(const std::string power);
-  void setKillMethod(const std::string kill_method);
+  void setCurrentHealth(int c_health);
+  void setMaxHealth(int m_health);
+  void setSelfDamage(int s_damage);
+  void setPersonDamage(int p_damage);
 
   void print() const;
-
 };
 
 #endif
